b19f162b77aa27ba56b32e6323bc61a9980fdb6b
suresh <naidu@gmail.com>
2019-11-24 12:26:27 +0530

    adding controllr

---
+++ Arro-Lymm/src/main/java/com/example/ArroLymm/controller/RestarantController.java
@@ -0,0 +1,11 @@
+package com.example.ArroLymm.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestarantController {
+
+	public String getRestarantName() {
+		return "Bhavachi";
+	}
+}
