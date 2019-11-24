cbf61c3721f6ba11420c0b05abfd5c3aaffda02f
siva <siva@gmail.com>
2019-11-24 11:41:22 +0530

    new project created

---
+++ Arro-Lymm/src/test/java/com/example/ArroLymm/ArroLymmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArroLymm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArroLymmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
